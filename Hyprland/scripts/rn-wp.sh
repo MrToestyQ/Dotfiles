4555386806085781a0168eba5d451f85692d2484
MrToestyQ <quinten.hogerheijde@gmail.com>
2025-02-10 17:41:18 +0100

    Add files via upload

--- Hyprland/scripts/rn-wp.sh
+++ Hyprland/scripts/rn-wp.sh
@@ -1,1 +1,8 @@
+ls -a /home/q/Pictures/wallpapers/ | \
+shuf -n1 | \
+xargs -I % swww img /home/q/Pictures/wallpapers/% \
+--transition-type grow \
+--transition-fps 60 \
+--transition-pos 1070,1050
+
 
